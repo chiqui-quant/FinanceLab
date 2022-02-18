f980044a306addc4d41e0136fbbab00ce1775248
Chiqui <christiansposi@gmail.com>
2022-02-18 19:55:46 +0100

    Initial commit with first script

---
+++ first_script.m
@@ -0,0 +1,9 @@
+% Understanding the basics
+close all %close all files
+clear all %clear all variables
+clc %clear shell
+
+% Let's create a vector
+v = [12 pi 0];
+% Let's print the vector
+disp(v);
