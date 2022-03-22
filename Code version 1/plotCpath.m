2c3d503140505cd6e8c8bb6d886218d9ef2f7329
Chiqui <christiansposi@gmail.com>
2022-03-22 12:48:07 +0100

    plotCpath and edited plotCZoomedOut)

--- Code version 1/plotCpath.m
+++ Code version 1/plotCpath.m
@@ -1,15 +1,12 @@
 % This function plots the simulated path of consumption over the
-% entire set of periods using a fixed range of variation
+% entire set of periods
+function plotCpath(cpath)
 
-global plotNumber T
-
-lowerboundforyaxis = 0; % min(cpath)*0.9
-upperboundforyaxis = 0.12; % max(cpath)*1.1
+global plotNumber
 
 plotNumber = plotNumber + 1;
 figure(plotNumber)
 plot(cpath,'LineWidth',2)
-axis([1 T lowerboundforyaxis upperboundforyaxis])
 xlabel('Age')
 ylabel('Consumption')
 title('Time path of consumption')
