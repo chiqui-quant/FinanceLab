61502d18a1e6c53abdfee301796a1fdbbdefd6be
Chiqui <christiansposi@gmail.com>
2022-03-23 15:25:30 +0100

    simulation without uncertainty

---
+++ Code version 1/simNoUncer.m
@@ -0,0 +1,25 @@
+% -------------------------------------------------------------------------
+% This function takes the starting assets, the policy function and the value
+% function and simulates paths of consumption, assets and value in an
+% environment where there is no uncertainty
+
+function [c,a,v] = simNoUncer(policyA1,V,startingA)
+   
+global T r
+global Agrid interpMethod
+
+% Initialize arrays that will hold the paths of income consumption, value and assets
+% Arguments for output
+c = NaN(T, 1);       % consumption
+v = NaN(T, 1);       % value
+a = NaN(T + 1, 1);    % starting assets for each period, decided one period ahead (T+1)
+
+% Paths using the initial condition and the policy and value functions
+a(1, 1) = startingA;   
+for t = 1:1:T                     % loop through time periods for a particular individual
+    v(t  , 1) = interp1(Agrid(t, :), V(t, :), a(t, 1), interpMethod, 'extrap');                               
+    a(t+1, 1) = interp1(Agrid(t, :), policyA1(t, :), a(t, 1), interpMethod, 'extrap');        
+    c(t  , 1) = a(t, 1)  - (a(t+1, 1)/(1+r));
+end   %t      
+
+end
